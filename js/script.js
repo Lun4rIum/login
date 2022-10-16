732000a0d8bc5b71a963961cbb76e4f5f587dc59
Lun4rIum <67378940+Lun4rIum@users.noreply.github.com>
2022-10-16 21:21:24 +0200

    Adding custom message

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 const login = document.getElementById("loginButton");
+
+
+login.addEventListener("click", function showLogs(){
+    document.getElementById("loginContainer").style.display ="none"
+    var pseudo =  document.getElementById("username").value;
+    var password =  document.getElementById("password").value;
+
+    var welcomePseudo = document.getElementById("welcomePseudo").innerHTML = pseudo
+    document.getElementById("welcome").style = "display: inline-flex;"
+});
